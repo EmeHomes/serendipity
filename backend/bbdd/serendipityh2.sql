aec401c39803ec51aa22a7e55e5a93c49534af93
EmeHomes <mscasas.94@gmail.com>
2020-06-20 20:42:50 +0200

    respaldo bbdd

--- backend/bbdd/serendipityh2.sql
+++ backend/bbdd/serendipityh2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 31-05-2020 a las 17:37:08
+-- Tiempo de generación: 20-06-2020 a las 19:28:00
 -- Versión del servidor: 10.4.11-MariaDB
 -- Versión de PHP: 7.4.5
 
